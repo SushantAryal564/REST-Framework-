dd2762e34d3c6dbfd5c84b09c76add6d3c7861fa
sushant <aryalsushant564@gmail.com>
2022-11-20 22:52:08 +0545

    Learnt about ModelViewSet

---
+++ ModelViewSet/api/views.py
@@ -0,0 +1,11 @@
+from .models import Student
+from .serializers import StudentSerializers
+from rest_framework import viewsets
+
+# class StudentModelViewSet(viewsets.ModelViewSet):
+#   queryset = Student.objects.all()
+#   serializer_class = StudentSerializers
+
+class StudentReadOnlyModelViewSet(viewsets.ReadOnlyModelViewSet):
+  queryset = Student.objects.all()
+  serializer_class = StudentSerializers
