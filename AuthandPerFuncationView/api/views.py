0d575e60078b63dbf1cd5323253576c5db67e825
sushant <aryalsushant564@gmail.com>
2022-11-22 05:20:52 +0545

    Learnt about authentication and permission in Function based view

---
+++ AuthandPerFuncationView/api/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.response import Response
+from .models import Student
+from .serializers import StudentSerializers
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+@api_view(['GET','POST','PUT','DELETE','PATCH'])
+@authentication_classes([BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def student_api(request, pk = None):
+  if request.method == "GET":
+    id = pk
+    if id is not None:
+      stu = Student.objects.get(pk=id)
+      serializer = StudentSerializers(stu)
+      return Response(serializer.data)
+    stu = Student.objects.all()
+    serializer = StudentSerializers(stu,many=True)
+    return Response(serializer.data)
+  
+  if request.method == "POST":
+    serializer = StudentSerializers(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response({'msg':'Data Created'})
+    return Response(serializer.errors)
+  
+  if request.method == "PUT":
+    id = pk
+    stu = Student.objects.get(pk = id)
+    serializer = StudentSerializers(stu,data=request.data,partial=True)
+    if serializer.is_valid():
+      serializer.save()
+      return Response({'msg':'complete data updated'})
+    return Response(serializer.errors)
+  
+  if request.method == "PATCH":
+    id = pk
+    stu = Student.objects.get(pk=id)
+    serializer = StudentSerializers(stu,data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response({'msg':'partial data updated'})
+    return Response(serializer.errors)
+  
+  if request.method == "DELETE":
+    id = pk
+    stu = Student.objects.get(pk=id)
+    stu.delete()
+    return Response({'msg':'Data deleted Successfully'})
+  
