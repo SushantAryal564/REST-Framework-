2e846edf80cbae06e6f040730b13958f3b47fecc
sushant <aryalsushant564@gmail.com>
2022-11-28 08:05:29 +0545

    Learnt about JWT auth

--- JWTAuth/JWTAuth/urls.py
+++ JWTAuth/JWTAuth/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path,include
 from api import views
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView,TokenVerifyView
+
 # creating Router Object
 router = DefaultRouter()
 #Register StudentViewSet with Router
@@ -11,5 +13,7 @@ router.register('studentapi',views.StudentRModelViewSet,basename = 'student')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include(router.urls)),
-    path('auth/',include('rest_framework.urls',namespace='rest_framework')),
+    path('gettoken/',TokenObtainPairView.as_view(),name="token_obtained_pair"),
+    path('refreshtoken/',TokenRefreshView.as_view(),name="token_refresh"),
+    path('verifytoken/',TokenVerifyView.as_view(),name="token_verify"),
 ]
--- JWTAuth/api/views.py
+++ JWTAuth/api/views.py
@@ -3,11 +3,11 @@ from .serializers import StudentSerializers
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from api.customauth import CustomAuthentication
-
+from rest_framework_simplejwt.authentication import JWTAuthentication
 class StudentRModelViewSet(viewsets.ModelViewSet):
   queryset = Student.objects.all()
   serializer_class = StudentSerializers
-  authentication_classes = [CustomAuthentication]
+  authentication_classes = [JWTAuthentication]
   permission_classes = [IsAuthenticated]
   
 
